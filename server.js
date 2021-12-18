01719e6ba693297bb345249993a90159aed81ecf
Eli Pagan <mbk8689@gmail.com>
2021-12-18 10:27:21 -0500

    added app.listen

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ const express = require('ecpress');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
