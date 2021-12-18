21c13a7bc40084b32df8772160554939ca93e4f5
Eli Pagan <mbk8689@gmail.com>
2021-12-18 15:31:54 -0500

    added routes to frontend html

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 const fs = require('fs');
 const path = require('path');
-const express = require('ecpress');
+const express = require('express');
+const { note } = require('./data/animals')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
 
 
 app.listen(PORT, () => {
