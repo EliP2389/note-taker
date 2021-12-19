805572322e51fa076289d74c5fcdf9895e9be42f
Eli Pagan <mbk8689@gmail.com>
2021-12-19 12:45:35 -0500

    adjustments

--- server.js
+++ server.js
@@ -1,31 +1,49 @@
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 const express = require('express');
-const { note } = require('./data/animals')
+const { notes } = require('./data/notes')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.static('public'));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
+// function filterByQuery(query, notesArray) {
+//     let filteredResults = notesArray;
+//     if (query.title) {
+//         filteredResults = filteredResults.filter(notes => notes.title === query.title);
+//     }
+//     if (query.text) {
+//         filteredResults = filteredResults.filter(notes => notes.text === query.text);
+//     }
+//     return filteredResults;
+// }
 
-
-
-
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+app.get('/api/notes', (req, res) => {
+    // let results = note;
+    // if (req.query) {
+    //     results = filterByQuery(req.query, results);
+    // }
+    res.json(notes);
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+
+
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'));
+// });
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/notes.html'));
+// });
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'));
+// });
 
 
 
